7b7381b4ced781986d3d8e84a798b8942fa3d4fe
takeshiho <takeshiho@kawadatakeshihonoMacBook-Pro.local>
2023-05-26 15:17:54 +0900

    wip: 23.5.26. B51

---
+++ b51/stack.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+string Q;
+stack<char> S;
+
+int main(){
+    for (int i=0;i<Q.size();i++){
+        S.push(Q.at(i));
+    }
+    for (int i=0;i<Q.size();i++){
+        if 
+    }
+}
