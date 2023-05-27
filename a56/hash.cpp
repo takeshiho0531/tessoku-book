78d886b369ac89b34519db9997fb3d77ffdf6f67
takeshiho <takeshiho@kawadatakeshihonoMacBook-Pro.local>
2023-05-27 17:41:51 +0900

    wip: 23.5.27 A56 できたところまで

---
+++ a56/hash.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int N;
+    int Q;
+    string S;
+    int a[200009]; int b[200009];int c[200009]; int d[200009];
+    int hash_ab[200009]; int hash_cd[200009];
+
+    for(int i=0;i<Q;i++){
+        cin>>a[i]>>b[i]>>c[i]>>d[i];
+    }
+    for(int i=0;i<Q;i++){
+    }
+
+}
